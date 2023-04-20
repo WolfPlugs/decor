caf78c4c2e62786fb9ccb0a7002048ab992156f3
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-04-19 21:00:12 -0400

    Finished Decor Plugin

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,39 @@
-import { Injector, Logger, webpack, common, types } from "replugged";
+import { Injector, common, webpack } from "replugged";
 
-const { users } = common
+const { users } = common;
 const inject = new Injector();
 
 const BASE_URL = "https://decor.fieryflames.dev";
 
 let usersCache: Map<string, string>;
-const fetchUsers = async (cache: RequestCache = "default") => usersCache = new Map(Object.entries(await fetch(BASE_URL + "/users.json", { cache }).then(c => c.json())));
-
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const fetchUsers = async (cache: RequestCache = "default") =>
+  (usersCache = new Map(
+    Object.entries(await fetch(`${BASE_URL}/users.json`, { cache }).then((c) => c.json())),
+  ));
 
 export async function start(): Promise<void> {
-  const AssetUtils = webpack.getByProps("getUserAvatarURL");
+  const AssetUtils = webpack.getBySource("getAvatarDecorationURL:");
 
-  inject.after(users, "getUser", (args, res) => {
-   if (res && usersCache?.has(res.id)) res.avatarDecoration = `decor_${usersCache?.get(res.id)}`;
+  inject.after(users, "getUser", (_, res) => {
+    if (res && usersCache?.has(res.id)) {
+      res.avatarDecoration = `decor_${usersCache?.get(res.id)}`;
+    }
     return res;
   });
 
-  inject.after(AssetUtils, "getAvatarDecorationURL", (args, res) => {
-  
-    console.log(args, res)
-    return res;
+  // @ts-expect-error - very lazy to type this
+  inject.after(AssetUtils, "NZ", ([{ avatarDecoration }]: string, _) => {
+    if (avatarDecoration?.startsWith("decor")) {
+      const parts = avatarDecoration?.split("_");
+      parts.shift();
+      return `${BASE_URL}/${parts.join("_")}.png`;
+    }
   });
 
-
-  await fetchUsers()
-  
+  await fetchUsers();
 }
 
 export function stop(): void {
   inject.uninjectAll();
-}
-
-
-
-
-async function fetchDecorations(decor) {
-  console.log(decor)
-  const res = await fetch(`${BASE_URL}/${decor}.png`);
-
 }
