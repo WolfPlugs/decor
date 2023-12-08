b05682e84318830870764909ab4837de84d727b2
Tharki-God <alokender018@gmail.com>
2023-12-08 19:23:41 +0530

    oki

--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -1,24 +1,29 @@
 import { common, webpack } from "replugged";
 import type { ComponentType, HTMLProps, PropsWithChildren } from "react";
 import { AvatarDecoration } from "../..";
+import Grid from "./Grid";
+const { React } = common;
 
-const { React }  = common
+type DecorationGridItemComponent = ComponentType<
+  PropsWithChildren<HTMLProps<HTMLDivElement>> & {
+    onSelect: () => void;
+    isSelected: boolean;
+  }
+>;
 
-type DecorationGridItemComponent = ComponentType<PropsWithChildren<HTMLProps<HTMLDivElement>> & {
-  onSelect: () => void,
-  isSelected: boolean,
-}>;
+type DecorationGridDecorationComponent = React.ComponentType<
+  HTMLProps<HTMLDivElement> & {
+    avatarDecoration: AvatarDecoration;
+    onSelect: () => void;
+    isSelected: boolean;
+  }
+>;
 
-type DecorationGridDecorationComponent = React.ComponentType<HTMLProps<HTMLDivElement> & {
-  avatarDecoration: AvatarDecoration;
-  onSelect: () => void,
-  isSelected: boolean,
-}>;
+export const AvatarDecorationModalPreview = React.memo(
+  webpack.getBySource("AvatarDecorationModalPreview"),
+);
 
-export const AvatarDecorationModalPreview = React.memo(webpack.getBySource("AvatarDecorationModalPreview"));
-
-export let DecorationGridItem: DecorationGridItemComponent;
-export const setDecorationGridItem = v => DecorationGridItem = v;
+export const DecorationGridItem: DecorationGridItemComponent = Grid;
 
 export let DecorationGridDecoration: DecorationGridDecorationComponent;
-export const setDecorationGridDecoration = v => DecorationGridDecoration = v;
+export const setDecorationGridDecoration = (v) => (DecorationGridDecoration = v);
